e83a2a501bddc67e0bfbae1cadaea6e481630b2f
AleksandrUsanov123 <aleksandr_usanov1994@mail.ru>
2024-04-18 19:23:47 +0300

    add Task01.java.

---
+++ src/Task01.java
@@ -0,0 +1,18 @@
+public class Task01 {
+    public static void main(String[] args) {
+        Sustem.out.println("Задание 1");
+        //
+        Задание 1
+        int x = 21;
+        int y = 4;
+        int q = x /
+                y;
+        int w = x % y;
+        System.out.println(x
+                + " / " + y + " = " + q + " и "+ w + "в остатке");
+
+        System.out.println("квадратныйкорень x =  " + Math.sqrt(x))
+
+    }
+
+}

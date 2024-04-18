c852172b43acaa4a83f35a1f37e9b66f935cec2b
AleksandrUsanov123 <aleksandr_usanov1994@mail.ru>
2024-04-18 19:46:23 +0300

    add Task02.java.

---
+++ src/Task02.java
@@ -0,0 +1,14 @@
+public class Task02 {
+    public static void main(String[] args) {
+        System.out.println("Задание 2");
+        int n = 254;
+        int x = n / 100; // 8
+        int y = n % 10; // 5
+        int z = n / 10; // 86
+        int zS = z % 10; // 6
+        int result = x + y + zS;
+        System.out.println("result");
+    }
+
+
+}

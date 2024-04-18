651850e850822462bb724d3cf8e680e62c0b623c
AleksandrUsanov123 <aleksandr_usanov1994@mail.ru>
2024-04-18 21:32:50 +0300

    add.Task10.java.

---
+++ src/Task10.java
@@ -0,0 +1,11 @@
+public class Task10 {
+    public static final int HEIHT = 185;
+    public static final int WEIHT = 80;
+
+    public static void main(String[] args) {
+        System.out.println("Задание 10");
+        int idealWeight = HEIGHT - 110; //75
+        int nWeight = idealWeight - WEIHT;
+        System.out.println("Нужно: " );
+    }
+}
